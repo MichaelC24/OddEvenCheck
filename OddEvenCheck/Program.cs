c4c8a0cb0b5356a5c089d48e6a350183e16e82ec
Michael Clemons <Mclemons@tql.com>
2024-06-12 13:37:58 -0400

    Add project files.

---
+++ OddEvenCheck/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OddEvenCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = 4;
+
+            //Console.WriteLine("Please input a number");
+            //string userInput = Console.ReadLine();
+
+            //input = int.Parse(userInput);
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine($"The number {input} is even");
+            }
+            else Console.WriteLine($"The number {input} is odd");
+
+        }
+    }
+}
